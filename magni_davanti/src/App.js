d9f51cea9ef77987fffa33e01e495242e921e832
Rohan Singh <rohannsingh98765@gmail.com>
2022-01-01 19:31:08 +0530

    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- magni_davanti/src/App.js
+++ magni_davanti/src/App.js
@@ -1,13 +1,24 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
   return (
+    
     <Routes>
         <Route path="login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Home />} />
     </Routes>
   );
 }
--- magni_davanti/src/index.js
+++ magni_davanti/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom'
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router> 
+  <Router>
     <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById('root'),
 );
-
-
