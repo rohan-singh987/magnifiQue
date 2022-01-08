257633254a4fa66c43fde344c82904bce76e45a7
Rohan Singh <rohannsingh98765@gmail.com>
2022-01-08 23:54:23 +0530

    minute changes
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- magni_davanti/src/components/Search.jsx
+++ magni_davanti/src/components/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Spinner from './Spinner'
 
 const Search = () => {
     return (
         <div>
-            Search
+            <Spinner/>
+            <h1 className=" flex items-center justify-center " >Search Box is still under devlopment...</h1>
         </div>
     )
 }
